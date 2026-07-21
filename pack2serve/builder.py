6d131694eb1c95ea9137bd8268f925e71e6339ff
Ha22yX <kicofy@vip.qq.com>
2026-07-21 22:54:22 +0800

    fix: isolate auto operator mods from servers

--- pack2serve/builder.py
+++ pack2serve/builder.py
@@ -71,6 +71,8 @@ KNOWN_CLIENT_ONLY_MOD_IDS = {
     "jecharacters",
     "just-zoom",
     "legacyskins",
+    "lan-operators",
+    "lanoperators",
     "mcwifipnp",
     "modmenu",
     "moremusic",
@@ -81,6 +83,8 @@ KNOWN_CLIENT_ONLY_MOD_IDS = {
     "smoothswapping",
     "sodium",
     "sodiumextras",
+    "auto-mod",
+    "auto-operator",
 }
 
 
